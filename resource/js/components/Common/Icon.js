7c007b29b07ac6ec13e341770d35b58c27d33e6f
Sotaro KARASAWA <sotaro.k@gmail.com>
2017-02-14 15:05:54 +0900

    Icon: spin support

--- resource/js/components/Common/Icon.js
+++ resource/js/components/Common/Icon.js
@@ -4,19 +4,25 @@ export default class Icon extends React.Component {
 
   render() {
     const name = this.props.name || null;
+    const isSpin = this.props.spin ? ' fa-spinner' : '';
 
     if (!name) {
       return '';
     }
 
     return (
-      <i className={"fa fa-" + name} />
+      <i className={`fa fa-${name} ${isSpin}`} />
     );
   }
 }
 
-// TODO: support spin, size and so far
+// TODO: support size and so far
 Icon.propTypes = {
   name: React.PropTypes.string.isRequired,
+  spin: React.PropTypes.bool,
+};
+
+Icon.defaltProps = {
+  spin: false,
 };
 
