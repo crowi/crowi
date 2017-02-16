e45782a09ccad326c39e1c79e5f5d0af6e9f4e29
Yuki Takei <yuki@weseek.co.jp>
2017-02-17 01:06:42 +0900

    Add a new replace process

--- resource/js/util/PreProcessor/Linker.js
+++ resource/js/util/PreProcessor/Linker.js
@@ -1,3 +1,4 @@
+var path = require('path');
 
 export default class Linker {
   process(markdown) {
@@ -5,6 +6,10 @@ export default class Linker {
     return markdown
       //.replace(/\s(https?:\/\/[\S]+)/g, ' <a href="$1">$1</a>') // リンク
       .replace(/\s<((\/[^>]+?){2,})>/g, ' <a href="$1">$1</a>') // ページ間リンク: <> でかこまれてて / から始まり、 / が2個以上
+      .replace(/\[\[(.*)\]\]/g, function(all, group1) {
+        var url = path.join(window.location.pathname, group1);
+        return `<a href="${url}">${group1}</a>`
+      });
       ;
   }
 }
