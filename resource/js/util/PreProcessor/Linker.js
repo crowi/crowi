338414e9fb9c5bca850e392a15384b1054abd3f7
Yuki Takei <yuki@weseek.co.jp>
2017-02-17 01:27:59 +0900

    Support alias

--- resource/js/util/PreProcessor/Linker.js
+++ resource/js/util/PreProcessor/Linker.js
@@ -6,9 +6,15 @@ export default class Linker {
     return markdown
       //.replace(/\s(https?:\/\/[\S]+)/g, ' <a href="$1">$1</a>') // リンク
       .replace(/\s<((\/[^>]+?){2,})>/g, ' <a href="$1">$1</a>') // ページ間リンク: <> でかこまれてて / から始まり、 / が2個以上
-      .replace(/\[\[(.*)\]\]/g, function(all, group1) {
-        var url = path.join(window.location.pathname, group1);
-        return `<a href="${url}">${group1}</a>`
+      .replace(/\[\[((.+)>)?(.+)\]\]/g, function(all, group1, group2, group3) {
+        // create url
+        // http(s) から始まっていればそのまま利用、そうでなければ window.location.pathname と連結
+        var url = (group3.match(/^https?:\/\//)) ? group3 : path.join(window.location.pathname, group3);
+        // determine alias string
+        // エイリアス指定があれば利用、そうでなければリンク指定文字列をそのまま利用
+        var alias = group2 ? group2 : group3;
+
+        return `<a href="${url}">${alias}</a>`
       });
       ;
   }
