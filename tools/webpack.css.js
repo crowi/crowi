f4ee72d94dba05b00b815f3cb49f8f33dd58d4d6
Sotaro KARASAWA <sotaro.k@gmail.com>
2018-05-14 01:15:13 +0900

    Fix: quotes

--- tools/webpack.css.js
+++ tools/webpack.css.js
@@ -1,34 +1,34 @@
 const path = require('path')
 const webpack = require('webpack')
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 const extractSass = new ExtractTextPlugin({
-  filename: "[name].css",
+  filename: '[name].css',
 });
 const config = {
   entry: {
     crowi: './resource/css/crowi.scss',
   },
   output: {
-    path: path.join(__dirname, "/../public/css"),
-    filename: "[name].css"
+    path: path.join(__dirname, '/../public/css'),
+    filename: '[name].css'
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: extractSass.extract({
           use: [{
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               url: false,
               minimize: isProduction,
             }
           }, {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               includePaths: [
                 './node_modules/bootstrap-sass/assets/stylesheets',
@@ -37,13 +37,13 @@ const config = {
               ]
             }
           }],
-          fallback: "style-loader"
+          fallback: 'style-loader'
         })
       },
       {
         test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
         use: [{
-          loader: "file-loader"
+          loader: 'file-loader'
         }]
       }
     ]
