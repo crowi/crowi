8f04f7bd9b61fb8fdc1a288fcc0e65284839f8fd
lightnet328 <lightnet328@gmail.com>
2018-06-22 12:48:54 +0900

    Improve usability

--- client/components/ExternalShare/SecretKeywordForm/SecretKeywordFormContainer.js
+++ client/components/ExternalShare/SecretKeywordForm/SecretKeywordFormContainer.js
@@ -1,32 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import { Button, InputGroup, Col, ControlLabel, Form, FormControl, FormGroup } from 'react-bootstrap'
+import { translate } from 'react-i18next'
+import { Button, InputGroup, Col, FormControl, FormGroup, HelpBlock } from 'react-bootstrap'
 
 import Icon from '../../Common/Icon'
 
-export default class SecretKeywordFormContainer extends React.Component {
+class SecretKeywordFormContainer extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
       secretKeyword: '',
+      error: {
+        status: false,
+        message: '',
+      },
+      usingIME: true,
     }
 
     this.setSecretKeyword = this.setSecretKeyword.bind(this)
     this.canSubmit = this.canSubmit.bind(this)
+    this.handleError = this.handleError.bind(this)
+    this.checkSecretKeyword = this.checkSecretKeyword.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleKeyUp = this.handleKeyUp.bind(this)
+  }
+
+  static propTypes = {
+    pageId: PropTypes.string,
+    crowi: PropTypes.object.isRequired,
   }
 
   setSecretKeyword(e) {
-    this.setState({ secretKeyword: e.target.value })
+    this.setState({ error: { status: false }, secretKeyword: e.target.value })
   }
 
   canSubmit() {
     return this.state.secretKeyword.length > 0
   }
 
-  handleSubmit(e) {
+  handleError() {
+    this.setState({ error: { status: true, message: this.props.t('Incorrect secret keyword') } })
+  }
+
+  checkSecretKeyword() {
     const { secretKeyword } = this.state
     const shareId = $('#secret-keyword-form-container').data('share-id')
     const _csrf = $('#secret-keyword-form-container').data('csrftoken')
@@ -34,7 +52,10 @@ export default class SecretKeywordFormContainer extends React.Component {
       .apiPost('/shares/secretKeyword.check', { _csrf, share_id: shareId, secret_keyword: secretKeyword })
       .then(({ hasAccessAuthority = false }) => {
         if (hasAccessAuthority) {
+          this.setState({ error: { status: false } })
           top.location.href = '/_share/' + shareId
+        } else {
+          this.handleError()
         }
       })
       .catch(err => {
@@ -42,18 +63,46 @@ export default class SecretKeywordFormContainer extends React.Component {
       })
   }
 
+  isEnterAndNotUsingIME(e) {
+    return e.keyCode == 13 && !this.state.usingIME
+  }
+
+  handleKeyPress(e) {
+    // `onKeyPress` event is not triggered if using IME.
+    this.setState({ usingIME: false })
+  }
+
+  handleKeyUp(e) {
+    if (this.isEnterAndNotUsingIME(e)) {
+      this.checkSecretKeyword()
+    }
+    this.setState({ usingIME: true })
+  }
+
+  handleSubmit(e) {
+    this.checkSecretKeyword()
+  }
+
   render() {
+    const { error } = this.state
     return (
       <Col lg={4} md={6} sm={8} xs={10}>
         <h4 className="page-header">ページの閲覧には秘密のキーワードが必要です</h4>
         <div>
-          <FormGroup controlId="secretKeyword">
+          <FormGroup controlId="secretKeyword" validationState={error.status ? 'error' : null}>
             <InputGroup>
               <InputGroup.Addon>
                 <Icon name="key" />
               </InputGroup.Addon>
-              <FormControl type="text" placeholder="Secret Keyword" onChange={this.setSecretKeyword} />
+              <FormControl
+                type="text"
+                placeholder="Secret Keyword"
+                onChange={this.setSecretKeyword}
+                onKeyPress={this.handleKeyPress}
+                onKeyUp={this.handleKeyUp}
+              />
             </InputGroup>
+            {error.status && <HelpBlock>{error.message}</HelpBlock>}
           </FormGroup>
 
           <FormGroup className="pull-right">
@@ -67,7 +116,4 @@ export default class SecretKeywordFormContainer extends React.Component {
   }
 }
 
-SecretKeywordFormContainer.propTypes = {
-  pageId: PropTypes.string,
-  crowi: PropTypes.object.isRequired,
-}
+export default translate()(SecretKeywordFormContainer)
