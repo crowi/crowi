64962cac7bd91634f66a5661dfa5aec7906e1130
lightnet328 <lightnet328@gmail.com>
2018-07-09 18:50:41 +0900

    Add options for specifying the page type in the search and display by type in the search suggestion

--- client/components/HeaderSearchBox/SearchSuggest.js
+++ client/components/HeaderSearchBox/SearchSuggest.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Icon from 'components/Common/Icon'
 import ListView from 'components/PageList/ListView'
 
 export default class SearchSuggest extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.renderList = this.renderList.bind(this)
+  }
+
+  renderList(title, icon, pages) {
+    return (
+      pages.length > 0 && (
+        <div className="grouped-page-list">
+          <h5>
+            <Icon name={icon} regular />
+            <span>{title}</span>
+          </h5>
+          <ListView pages={pages} />
+        </div>
+      )
+    )
+  }
+
   render() {
     if (!this.props.focused) {
       return <div />
@@ -25,7 +46,10 @@ export default class SearchSuggest extends React.Component {
       )
     }
 
-    if (this.props.searchedPages.length < 1) {
+    const numberOfResults = Object.values(this.props.searchedPages)
+      .map((r = []) => r.length)
+      .reduce((p, c) => p + c, 0)
+    if (numberOfResults < 1) {
       if (this.props.searchingKeyword !== '') {
         return (
           <div className="search-suggest" id="search-suggest">
@@ -36,16 +60,20 @@ export default class SearchSuggest extends React.Component {
       return <div />
     }
 
+    const { portalPages, publicPages, userPages } = this.props.searchedPages
+
     return (
       <div className="search-suggest" id="search-suggest">
-        <ListView pages={this.props.searchedPages} />
+        {this.renderList('Portal', 'circle', portalPages)}
+        {this.renderList('Public', 'file', publicPages)}
+        {this.renderList('User', 'user', userPages)}
       </div>
     )
   }
 }
 
 SearchSuggest.propTypes = {
-  searchedPages: PropTypes.array.isRequired,
+  searchedPages: PropTypes.object.isRequired,
   searchingKeyword: PropTypes.string.isRequired,
   searching: PropTypes.bool.isRequired,
   searchError: PropTypes.object,
@@ -53,7 +81,7 @@ SearchSuggest.propTypes = {
 }
 
 SearchSuggest.defaultProps = {
-  searchedPages: [],
+  searchedPages: {},
   searchingKeyword: '',
   searchError: null,
   searching: false,
