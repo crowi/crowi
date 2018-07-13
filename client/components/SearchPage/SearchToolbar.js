3271e0e0379c6658cc81b09d10ea40b2f6d678d1
lightnet328 <lightnet328@gmail.com>
2018-07-13 09:13:20 +0900

    Translate

--- client/components/SearchPage/SearchToolbar.js
+++ client/components/SearchPage/SearchToolbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { translate } from 'react-i18next'
 import { Nav, NavItem } from 'react-bootstrap'
 import Icon from 'components/Common/Icon'
 
-export default class SearchToolbar extends React.Component {
+class SearchToolbar extends React.Component {
   constructor() {
     super()
 
@@ -19,25 +20,30 @@ export default class SearchToolbar extends React.Component {
   }
 
   render() {
+    const { t } = this.props
     return (
       <div className="search-toolbar row">
         <div className="search-meta col-xs-4">
           <h3 className="search-keyword">{this.props.keyword}</h3>
-          <small className="text-muted">{this.props.total} 件</small>
+          <small className="text-muted">{t('search.toolbar.results', { value: this.props.total })}</small>
         </div>
         <nav className="search-navbar col-xs-8">
           <Nav bsClass="nav navbar-nav" activeKey={this.getActiveType()} onSelect={this.props.changeType}>
             <NavItem eventKey={this.searchTypes[0]} href="#">
-              <Icon name="th" />すべて
+              <Icon name="th" />
+              {t('page_types.all')}
             </NavItem>
             <NavItem eventKey={this.searchTypes[1]} href="#">
-              <Icon name="circle" regular />ポータル
+              <Icon name="circle" regular />
+              {t('page_types.portal')}
             </NavItem>
             <NavItem eventKey={this.searchTypes[2]} href="#">
-              <Icon name="file" regular />パブリック
+              <Icon name="file" regular />
+              {t('page_types.public')}
             </NavItem>
             <NavItem eventKey={this.searchTypes[3]} href="#">
-              <Icon name="user" regular />ユーザー
+              <Icon name="user" regular />
+              {t('page_types.user')}
             </NavItem>
           </Nav>
         </nav>
@@ -51,9 +57,12 @@ SearchToolbar.propTypes = {
   type: PropTypes.string,
   total: PropTypes.number,
   changeType: PropTypes.func,
+  t: PropTypes.func.isRequired,
 }
 SearchToolbar.defaultProps = {
   keyword: '',
   type: '',
   total: 0,
 }
+
+export default translate()(SearchToolbar)
