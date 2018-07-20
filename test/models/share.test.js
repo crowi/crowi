ff7d72718306017148e8dace41e6369b88051a37
lightnet328 <lightnet328@gmail.com>
2018-07-20 12:33:07 +0900

    Fix test

--- test/models/share.test.js
+++ test/models/share.test.js
@@ -9,6 +9,7 @@ chai.use(chaiAsPromised)
 chai.use(sinonChai)
 
 describe('Share', () => {
+  const User = utils.models.User
   const Page = utils.models.Page
   const Share = utils.models.Share
   const conn = utils.mongoose.connection
@@ -16,26 +17,31 @@ describe('Share', () => {
   let page
 
   before(async () => {
+    await User.remove({})
     const createdUsers = await testDBUtil.generateFixture(conn, 'User', [
       { name: faker.name.findName(), username: faker.internet.userName(), email: faker.internet.email() },
     ])
     user = createdUsers[0]
-  })
 
-  beforeEach(async () => {
     await Page.remove({})
     const createdPages = await testDBUtil.generateFixture(conn, 'Page', [
       { path: '/' + faker.lorem.slug(), grant: Page.GRANT_PUBLIC, grantedUsers: [user], creator: user },
+      { path: '/' + faker.lorem.slug(), grant: Page.GRANT_PUBLIC, grantedUsers: [user], creator: user },
     ])
     page = createdPages[0]
-    await Share.delete({})
+
+    await Share.remove({})
+  })
+
+  afterEach(async () => {
+    await Share.remove({})
   })
 
   describe('.create', () => {
     context('Create shares', () => {
       it('should be able to create only one active share per page', async () => {
-        expect(Share.create(page._id, user)).to.be.eventually.an.instanceof(Share)
-        expect(Share.create(page._id, user)).to.be.rejected
+        await expect(Share.create(page._id, user)).to.be.eventually.an.instanceof(Share)
+        await expect(Share.create(page._id, user)).to.be.rejected
       })
     })
   })
@@ -48,7 +54,7 @@ describe('Share', () => {
       })
 
       it('should inactivate share', async () => {
-        expect(Share.delete(page._id, user)).to.eventually.have.property('status', Share.STATUS_INACTIVE)
+        await expect(Share.delete(share)).to.eventually.have.property('status', Share.STATUS_INACTIVE)
       })
     })
   })
