4d20473753ecebab80ee41f5ac88581a0c7aec83
lightnet328 <lightnet328@gmail.com>
2018-10-11 13:52:00 +0900

    Use a dropdown menu instead of buttons on extra small devices

--- client/components/SearchPage/SearchToolbar.js
+++ client/components/SearchPage/SearchToolbar.js
@@ -1,53 +1,60 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { translate } from 'react-i18next'
-import { Nav, NavItem } from 'react-bootstrap'
 import Icon from 'components/Common/Icon'
+import SearchTypeNav from 'components/SearchPage/SearchTypeNav/SearchTypeNav'
 
 class SearchToolbar extends React.Component {
   constructor(props) {
     super(props)
 
-    this.searchTypes = ['', 'portal', 'public', 'user']
+    const { t } = this.props
+    this.searchTypes = [
+      {
+        key: '',
+        icon: <Icon name="th" />,
+        name: t('page_types.all'),
+      },
+      {
+        key: 'portal',
+        icon: <Icon name="circle" regular />,
+        name: t('page_types.portal'),
+      },
+      {
+        key: 'public',
+        icon: <Icon name="file" regular />,
+        name: t('page_types.public'),
+      },
+      {
+        key: 'user',
+        icon: <Icon name="user" regular />,
+        name: t('page_types.user'),
+      },
+    ]
 
     this.getActiveType = this.getActiveType.bind(this)
   }
 
   getActiveType() {
     const defaultType = this.searchTypes[0]
-    const { type } = this.props
-    return this.searchTypes.includes(type) ? type : defaultType
+    const searchTypes = this.searchTypes.reduce((object, { key, icon, name }) => ({ ...object, [key]: { key, icon, name } }), {})
+    const { type: searchType } = this.props
+    return searchType in searchTypes ? searchTypes[searchType] : defaultType
   }
 
   render() {
-    const { t } = this.props
+    const { t, changeType } = this.props
+    const activeType = this.getActiveType()
     return (
       <div className="search-toolbar row">
-        <div className="search-meta col-xs-4">
+        <div className="search-meta col-md-4">
           <h3 className="search-keyword">{this.props.keyword}</h3>
           <small className="text-muted">
             {(this.props.searching && <Icon name="spinner" spin />) || t('search.toolbar.results', { value: this.props.total })}
           </small>
         </div>
-        <nav className="search-navbar col-xs-8">
-          <Nav bsClass="nav navbar-nav" activeKey={this.getActiveType()} onSelect={this.props.changeType}>
-            <NavItem eventKey={this.searchTypes[0]} href="#">
-              <Icon name="th" />
-              {t('page_types.all')}
-            </NavItem>
-            <NavItem eventKey={this.searchTypes[1]} href="#">
-              <Icon name="circle" regular />
-              {t('page_types.portal')}
-            </NavItem>
-            <NavItem eventKey={this.searchTypes[2]} href="#">
-              <Icon name="file" regular />
-              {t('page_types.public')}
-            </NavItem>
-            <NavItem eventKey={this.searchTypes[3]} href="#">
-              <Icon name="user" regular />
-              {t('page_types.user')}
-            </NavItem>
-          </Nav>
+        <nav className="search-navbar col-md-8">
+          <SearchTypeNav searchTypes={this.searchTypes} activeType={activeType} changeType={changeType} />
         </nav>
       </div>
     )
