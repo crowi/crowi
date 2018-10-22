1f61527ec86f93fbdf8e39d83e3d12cf493081dd
otofune <otofune@gmail.com>
2018-10-22 13:02:11 +0900

    add findByUser test

--- test/models/team.test.js
+++ test/models/team.test.js
@@ -4,13 +4,21 @@ const sinonChai = require('sinon-chai')
 const utils = require('../utils.js')
 chai.use(sinonChai)
 
-const { ObjectId } = require('mongoose').Schema.Types
+const crypto = require('crypto')
 
 describe('Page', () => {
   const { User, Team } = utils.models
   const conn = utils.mongoose.connection
   let users = []
   let team = null
+
+  const createTeam = (...users) => {
+    const t = new Team({
+      handle: crypto.randomBytes(16).toString('hex'),
+      users
+    })
+    return t.save()
+  }
 
   before(async () => {
     const userFixture = [
@@ -24,30 +32,38 @@ describe('Page', () => {
     await User.remove({})
   })
 
-  beforeEach(async () => {
-    const t = new Team({
-      handle: 'anon',
-      users: []
-    })
-    team = await t.save()
-  })
-
   afterEach(async () => {
-    await team.remove()
+    await Team.remove({})
   })
 
-  describe('#addUser, #deleteUser', () => {
-    it('mixed', async () => {
+  it('#addUser, #deleteUser', async () => {
+      const team = await createTeam()
+
       const team1 = await team.addUser(...users)
-      expect(team1.users.length).to.be.equal(2)
+      expect(team1.users).lengthOf(2)
 
       const team2 = await team1.deleteUser(users[0])
-      expect(team2.users.length).to.be.equal(1)
+      expect(team2.users).lengthOf(1)
       const team3 = await team1.deleteUser(users[0])
-      expect(team3.users.length).to.be.equal(1)
+      expect(team3.users).lengthOf(1)
 
       const team4 = await team1.deleteUser(users[1])
-      expect(team4.users.length).to.be.equal(0)
-    })
+      expect(team4.users).lengthOf(0)
+  })
+
+  it('#findByUser', async () => {
+    await createTeam(...users)
+    const team_0 = await createTeam(users[0])
+    const team_1 = await createTeam(users[1])
+
+    const teams_0 = await Team.findByUser(users[0])
+    expect(teams_0).lengthOf(2)
+    // ここらへんの assert うまいことできんかな
+    expect(teams_0.map(team => team._id.toString())).that.does.not.include(team_1.toString())
+
+    const teams_1 = await Team.findByUser(users[1])
+    expect(teams_1).lengthOf(2)
+    // ここらへんの assert うまいことできんかな
+    expect(teams_1.map(team => team._id.toString())).that.does.not.include(team_0.toString())
   })
 })
