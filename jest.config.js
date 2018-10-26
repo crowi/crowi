6ade5826e2fccb848db2f1fce10246b8a7f20bb2
lightnet328 <lightnet328@gmail.com>
2018-10-26 20:32:01 +0900

    Import regeneratorRuntime

--- jest.config.js
+++ jest.config.js
@@ -115,7 +115,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ['./node_modules/regenerator-runtime/runtime'],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
   setupTestFrameworkScriptFile: './test/bootstrap.js',
