5dde88e08a1752eabd4870e9b6c34d2b38763b6d
otofune <otofune@gmail.com>
2018-11-08 18:55:24 +0900

    fix CreateTeam component

--- client/components/CreateTeam.js
+++ client/components/CreateTeam.js
@@ -4,6 +4,13 @@ import PropTypes from 'prop-types'
 import { Modal } from 'react-bootstrap'
 
 export default class CreateTeam extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.onModalCloseClicked = this.props.onModalCloseClicked
+    this.crowi = this.props.crowi
+  }
+
   render() {
     return (
       <Modal>
@@ -13,6 +20,7 @@ export default class CreateTeam extends React.Component {
   }
 }
 
-CreateTeam.PropTypes = {
+CreateTeam.propTypes = {
   crowi: PropTypes.object.isRequired,
+  onModalCloseClicked: PropTypes.func.isRequired,
 }
