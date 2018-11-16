0da6df25a323d12b7f8cb65cb3d1685672d3c46c
lightnet328 <lightnet328@gmail.com>
2018-11-16 19:10:13 +0900

    Rewrite in Jest

--- test/models/watcher.test.js
+++ test/models/watcher.test.js
@@ -1,9 +1,4 @@
-const chai = require('chai')
-const expect = chai.expect
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
 const utils = require('../utils.js')
-chai.use(sinonChai)
 
 describe('Watcher', function() {
   const Watcher = utils.models.Watcher
@@ -11,17 +6,17 @@ describe('Watcher', function() {
   const ObjectId = mongoose.Types.ObjectId
 
   describe('.upsertWatcher', function() {
-    context('valid parameters', function() {
+    describe('valid parameters', function() {
       it('should create', async function() {
         const userId = ObjectId()
         const targetId = ObjectId()
 
         try {
           const watcher = await Watcher.upsertWatcher(userId, 'Page', targetId, Watcher.STATUS_WATCH)
-          expect(watcher.user.toString()).to.be.equal(userId.toString())
-          expect(watcher.targetModel).to.be.equal('Page')
-          expect(watcher.target.toString()).to.be.equal(targetId.toString())
-          expect(watcher.status).to.be.equal(Watcher.STATUS_WATCH)
+          expect(watcher.user.toString()).toBe(userId.toString())
+          expect(watcher.targetModel).toBe('Page')
+          expect(watcher.target.toString()).toBe(targetId.toString())
+          expect(watcher.status).toBe(Watcher.STATUS_WATCH)
         } catch (err) {
           throw new Error(err)
         }
