9ab052b608bc1820786091f43a70fc39c5ef0c99
lightnet328 <lightnet328@gmail.com>
2018-12-14 20:42:20 +0900

    Define global types

---
+++ client/types/global.d.ts
@@ -0,0 +1,25 @@
+import Crowi from 'client/crowi'
+import crowi from 'client/util/Crowi'
+import crowiAuth from 'client/util/CrowiAuth'
+import crowiRenderer from 'client/util/CrowiRenderer'
+import JQuery from 'jquery'
+
+declare global {
+  interface Window {
+    Crowi: Crowi
+    crowi: crowi
+    crowiAuth: crowiAuth
+    crowiRenderer: crowiRenderer
+    Reveal: {}
+    MathJax: {}
+    inlineAttachment: any
+  }
+
+  interface Error {
+    info?: {}
+  }
+
+  interface JQuery {
+    selection: (mode?: string, opts?: {}) => any
+  }
+}
