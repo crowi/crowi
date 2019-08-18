267cc3f44e77939d3e7a4c064e4319db1c9d1752
lightnet328 <lightnet328@gmail.com>
2019-08-19 02:23:28 +0900

    Add colors of Bootstrap

---
+++ client/constants/colors.ts
@@ -0,0 +1,50 @@
+// Imported from https://github.com/twbs/bootstrap/blob/v4.3.1/scss/_variables.scss
+
+export const white = '#fff'
+export const gray = {
+  100: '#f8f9fa',
+  200: '#e9ecef',
+  300: '#dee2e6',
+  400: '#ced4da',
+  500: '#adb5bd',
+  600: '#6c757d',
+  700: '#495057',
+  800: '#343a40',
+  900: '#212529',
+}
+export const black = '#000'
+
+export const blue = '#007bff'
+export const indigo = '#6610f2'
+export const purple = '#6f42c1'
+export const pink = '#e83e8c'
+export const red = '#dc3545'
+export const orange = '#fd7e14'
+export const yellow = '#ffc107'
+export const green = '#28a745'
+export const teal = '#20c997'
+export const cyan = '#17a2b8'
+
+export const colors = {
+  blue,
+  indigo,
+  purple,
+  pink,
+  red,
+  orange,
+  yellow,
+  green,
+  teal,
+  cyan,
+  white,
+  grayLight: gray[100],
+  gray: gray[600],
+  grayDark: gray[800],
+}
+
+export const success = green
+export const info = cyan
+export const warning = yellow
+export const danger = red
+export const light = gray[100]
+export const dark = gray[800]
