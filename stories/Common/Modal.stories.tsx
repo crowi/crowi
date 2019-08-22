d123f2bca481cf02bc34a1ed896ff78a7e8422f0
Tadao Iseki <saitoeku3@gmail.com>
2019-08-23 01:26:13 +0900

    Add a Storybook example

---
+++ stories/Common/Modal.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Modal from '../../client/components/Common/Modal'
+
+storiesOf('Common', module).add('Modal', () => <Modal />)
