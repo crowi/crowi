1128cc31c8d4ff202b29bb5aa54261489ec9ce89
lightnet328 <lightnet328@gmail.com>
2019-08-29 03:52:09 +0900

    Fix test

--- client/components/Common/Icon.test.tsx
+++ client/components/Common/Icon.test.tsx
@@ -3,14 +3,12 @@ import { shallow } from 'enzyme'
 import Icon from './Icon'
 
 describe('Icon', () => {
-  describe('toPathName', () => {
-    it('should convert icon name to path name', () => {
+  describe('props', () => {
+    it('should have same props', () => {
       const wrapper = shallow(<Icon name="account" />)
       const instance = wrapper.instance()
 
-      expect(instance.toPathName('account')).toBe('mdiAccount')
-      expect(instance.toPathName('account-alert')).toBe('mdiAccountAlert')
-      expect(instance.toPathName('account-alert-outline')).toBe('mdiAccountAlertOutline')
+      expect(instance.props().name).toBe('account')
     })
   })
 })
