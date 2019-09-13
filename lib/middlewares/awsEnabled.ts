0b7a8a4bfea9e13416dea79b353a67faf6d408c2
lightnet328 <lightnet328@gmail.com>
2019-09-14 01:42:31 +0900

    Divide AWS settings by domain

--- lib/middlewares/awsEnabled.ts
+++ lib/middlewares/awsEnabled.ts
@@ -2,10 +2,10 @@ export default () => {
   return (req, res, next) => {
     var config = req.config
     if (
-      config.crowi['aws:region'] !== '' &&
-      config.crowi['aws:bucket'] !== '' &&
-      config.crowi['aws:accessKeyId'] !== '' &&
-      config.crowi['aws:secretAccessKey'] !== ''
+      config.crowi['upload:aws:region'] !== '' &&
+      config.crowi['upload:aws:bucket'] !== '' &&
+      config.crowi['upload:aws:accessKeyId'] !== '' &&
+      config.crowi['upload:aws:secretAccessKey'] !== ''
     ) {
       req.flash('globalError', 'AWS settings required to use this function. Please ask the administrator.')
       return res.redirect('/')
