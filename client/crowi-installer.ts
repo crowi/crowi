d5a31cd51b60ba020a716c40860bedafd033fd7f
lightnet328 <lightnet328@gmail.com>
2019-09-28 02:57:46 +0900

    Use renderIcon function to render svg path directly

--- client/crowi-installer.ts
+++ client/crowi-installer.ts
@@ -1,3 +1,6 @@
+import { alert } from 'components/Common/Icons'
+import renderIcon from 'common/functions/renderIcon'
+
 $(function() {
   $('#register-form input[name="registerForm[username]"]').change(function(e) {
     var username = $(this).val()
@@ -6,7 +9,7 @@ $(function() {
 
     $.getJSON('/_api/check_username', { username: username }, function(json) {
       if (!json.valid) {
-        $('#help-block-username').html('<i class="mdi mdi-alert"></i>このユーザーIDは利用できません。<br>')
+        $('#help-block-username').html(`${renderIcon(alert)}このユーザーIDは利用できません。<br>`)
         $('#input-group-username').addClass('has-error')
       }
     })
