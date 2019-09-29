50f5c4e058f26c828aae81ad1f43dcd36f6123c9
lightnet328 <lightnet328@gmail.com>
2019-09-30 01:15:07 +0900

    Use fixture generators

--- client/components/PageList/ListView.stories.tsx
+++ client/components/PageList/ListView.stories.tsx
@@ -1,58 +1,32 @@
 import React from 'react'
 import faker from 'faker'
+import { createPage } from 'client/fixtures'
 import ListView from './ListView'
 import { Revision } from 'client/types/crowi'
 
 export default { title: 'PageList/ListView' }
 
-const createUser = () => ({
-  _id: faker.random.uuid(),
-  image: '',
-  name: faker.name.findName(),
-  username: faker.internet.userName(),
-})
-
 const pages = [
   {
-    _id: faker.random.uuid(),
-    path: '/user/sotarok',
-    revision: {} as Revision,
-    redirectTo: '',
-    status: '',
-    creator: createUser(),
-    lastUpdateUser: createUser(),
-    liker: Array(50),
-    seenUsers: Array(1000),
-    commentCount: 0,
-    bookmarkCount: 50,
+    ...createPage({
+      path: '/user/sotarok',
+      liker: Array(50),
+      seenUsers: Array(1000),
+      bookmarkCount: 50,
+    }),
     grant: 1,
   },
   {
-    _id: faker.random.uuid(),
-    path: '/crowi/',
-    revision: {} as Revision,
-    redirectTo: '',
-    status: '',
-    creator: createUser(),
-    lastUpdateUser: createUser(),
-    liker: Array(5),
-    seenUsers: Array(10),
-    commentCount: 0,
-    bookmarkCount: 20,
+    ...createPage({
+      path: '/crowi/',
+      liker: Array(5),
+      seenUsers: Array(10),
+      bookmarkCount: 20,
+    }),
     grant: 1,
   },
   {
-    _id: faker.random.uuid(),
-    path: '/user/lightnet328/memo/2019/09/28',
-    revision: {} as Revision,
-    redirectTo: '',
-    status: '',
-    creator: createUser(),
-    lastUpdateUser: createUser(),
-    liker: [],
-    seenUsers: [],
-    commentCount: 0,
-    bookmarkCount: 0,
+    ...createPage({ path: '/user/lightnet328/memo/2019/09/28' }),
     grant: 4,
   },
 ]
