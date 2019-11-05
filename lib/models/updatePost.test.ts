e055dd969aecb813746762da9b7bdfe0a9e7f7cb
Sotaro KARASAWA <sotaro.k@gmail.com>
2019-11-05 09:31:07 +0900

    Fix lint

--- lib/models/updatePost.test.ts
+++ lib/models/updatePost.test.ts
@@ -9,40 +9,46 @@ describe('UpdatePost', () => {
 
   describe('.createPrefixesByPathPattern', () => {
     describe('with a path', () => {
-      test('should return right patternPrfixes', done => {
-        expect(UpdatePost.createPrefixesByPathPattern('/*')).toEqual(['*', '*'])
-        expect(UpdatePost.createPrefixesByPathPattern('/user/*/日報*')).toEqual(['user', '*'])
-        expect(UpdatePost.createPrefixesByPathPattern('/project/hoge/*')).toEqual(['project', 'hoge'])
-        expect(UpdatePost.createPrefixesByPathPattern('/*/MTG/*')).toEqual(['*', 'MTG'])
-        expect(UpdatePost.createPrefixesByPathPattern('自己紹介')).toEqual(['*', '*'])
-        expect(UpdatePost.createPrefixesByPathPattern('/user/aoi/メモ/2016/02/10/xxx')).toEqual(['user', 'aoi'])
+      test('should return right patternPrfixes', () => {
+        return new Promise(resolve => {
+          expect(UpdatePost.createPrefixesByPathPattern('/*')).toEqual(['*', '*'])
+          expect(UpdatePost.createPrefixesByPathPattern('/user/*/日報*')).toEqual(['user', '*'])
+          expect(UpdatePost.createPrefixesByPathPattern('/project/hoge/*')).toEqual(['project', 'hoge'])
+          expect(UpdatePost.createPrefixesByPathPattern('/*/MTG/*')).toEqual(['*', 'MTG'])
+          expect(UpdatePost.createPrefixesByPathPattern('自己紹介')).toEqual(['*', '*'])
+          expect(UpdatePost.createPrefixesByPathPattern('/user/aoi/メモ/2016/02/10/xxx')).toEqual(['user', 'aoi'])
 
-        done()
+          resolve()
+        })
       })
     })
   })
 
   describe('.getRegExpByPattern', () => {
     describe('with a pattern', () => {
-      test('should return right regexp', done => {
-        expect(UpdatePost.getRegExpByPattern('/*')).toEqual(/^\/.*/)
-        expect(UpdatePost.getRegExpByPattern('/user/*/日報*')).toEqual(/^\/user\/.*\/日報.*/)
-        expect(UpdatePost.getRegExpByPattern('/project/hoge/*')).toEqual(/^\/project\/hoge\/.*/)
-        expect(UpdatePost.getRegExpByPattern('/*/MTG/*')).toEqual(/^\/.*\/MTG\/.*/)
-        expect(UpdatePost.getRegExpByPattern('自己紹介')).toEqual(/^\/.*自己紹介.*/)
-        expect(UpdatePost.getRegExpByPattern('/user/aoi/メモ/2016/02/10/xxx')).toEqual(/^\/user\/aoi\/メモ\/2016\/02\/10\/xxx/)
-        done()
+      test('should return right regexp', () => {
+        return new Promise(resolve => {
+          expect(UpdatePost.getRegExpByPattern('/*')).toEqual(/^\/.*/)
+          expect(UpdatePost.getRegExpByPattern('/user/*/日報*')).toEqual(/^\/user\/.*\/日報.*/)
+          expect(UpdatePost.getRegExpByPattern('/project/hoge/*')).toEqual(/^\/project\/hoge\/.*/)
+          expect(UpdatePost.getRegExpByPattern('/*/MTG/*')).toEqual(/^\/.*\/MTG\/.*/)
+          expect(UpdatePost.getRegExpByPattern('自己紹介')).toEqual(/^\/.*自己紹介.*/)
+          expect(UpdatePost.getRegExpByPattern('/user/aoi/メモ/2016/02/10/xxx')).toEqual(/^\/user\/aoi\/メモ\/2016\/02\/10\/xxx/)
+          resolve()
+        })
       })
     })
   })
 
   describe('.normalizeChannelName', () => {
     describe('with a channel name', () => {
-      test('should return true', done => {
-        expect(UpdatePost.normalizeChannelName('#pj-hoge')).toBe('pj-hoge')
-        expect(UpdatePost.normalizeChannelName('pj-hoge')).toBe('pj-hoge')
+      test('should return true', () => {
+        return new Promise(resolve => {
+          expect(UpdatePost.normalizeChannelName('#pj-hoge')).toBe('pj-hoge')
+          expect(UpdatePost.normalizeChannelName('pj-hoge')).toBe('pj-hoge')
 
-        done()
+          resolve()
+        })
       })
     })
   })
