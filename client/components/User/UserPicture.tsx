a12d906bc0d1e8b87c214e3ee0f4c9cba9059a0c
lightnet328 <lightnet328@gmail.com>
2019-12-05 01:10:33 +0900

    Refactor UserPicture component

--- client/components/User/UserPicture.tsx
+++ client/components/User/UserPicture.tsx
@@ -1,36 +1,18 @@
-import React from 'react'
+import React, { FC } from 'react'
+import classNames from 'classnames'
 import { User } from 'client/types/crowi'
 import { CommonProps } from 'client/types/component'
 import { getUserPicture } from 'client/services/user'
 
 type Props = CommonProps & {
   user: User | undefined
-  size: string
+  size?: 'lg' | 'sm' | 'xs'
 }
 
-// TODO UserComponent?
-export default class UserPicture extends React.Component<Props> {
-  static defaultProps = {
-    user: {},
-    size: null,
-  }
+const UserPicture: FC<Props> = ({ user, size, className, ...props }) => {
+  const sizeClassName = size ? `picture-${size}` : null
 
-  getClassName() {
-    const className = this.props.className ? [] : ([] as string[])
-
-    className.push('picture')
-    className.push('picture-rounded')
-
-    if (this.props.size) {
-      className.push('picture-' + this.props.size)
-    }
-
-    return className.join(' ')
-  }
-
-  render() {
-    const { user, size, className, ...props } = this.props
-
-    return <img src={getUserPicture(user)} alt={user ? user.username : ''} className={this.getClassName()} {...props} />
-  }
+  return <img className={classNames('picture', 'picture-rounded', sizeClassName, className)} src={getUserPicture(user)} alt={user?.username || ''} {...props} />
 }
+
+export default UserPicture
