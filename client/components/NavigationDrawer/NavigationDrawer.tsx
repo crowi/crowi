fd845d07ff40cd4c63b70b340244133b1b1365c0
lightnet328 <lightnet328@gmail.com>
2019-12-05 01:01:15 +0900

    Migrate styles to styled-component

--- client/components/NavigationDrawer/NavigationDrawer.tsx
+++ client/components/NavigationDrawer/NavigationDrawer.tsx
@@ -1,9 +1,69 @@
 import React, { FC } from 'react'
+import styled, { keyframes } from 'styled-components'
 import { useTranslation } from 'react-i18next'
 import Icon from 'client/components/Common/Icon'
 
 import Crowi from 'client/util/Crowi'
 import { getUserPicture, getUserPageRoot } from 'client/services/user'
+
+const SlideIn = keyframes`
+  100% {
+    transform: translateX(0%);
+  }
+`
+
+const NavigationDrawerContainer = styled.div`
+  display: block;
+  width: 300px;
+  position: fixed;
+  top: 3px;
+  bottom: 0;
+  z-index: 1200;
+  box-shadow: 0 0 20px 5px rgba(0, 0, 0, 0.1);
+  background: #fcfcfc;
+  transform: translateX(-100%);
+  animation: ${SlideIn} 0.2s forwards;
+`
+
+const StyledNavigationDrawer = styled.div`
+  height: 100%;
+  overflow-y: scroll;
+`
+
+const UserProfile = styled.div`
+  padding: 1em;
+`
+
+const Names = styled.div`
+  padding-top: 1em;
+  padding-bottom: 1em;
+`
+
+const Name = styled.span`
+  font-size: 1.2em;
+  font-weight: 600;
+`
+
+const Username = styled.span`
+  font-size: 0.9em;
+  color: #777;
+`
+
+const Menu = styled.ul`
+  position: relative;
+  width: calc(100% - 1em);
+  padding-left: 1em;
+`
+
+const MenuItem = styled.li`
+  list-style: none;
+  padding-bottom: 0.4em;
+`
+
+const Divider = styled(MenuItem)`
+  border-bottom: solid 1px #f0f0f0;
+  margin-bottom: 0.4em;
+`
 
 interface Props {
   crowi: Crowi
@@ -15,8 +75,8 @@ const NavigationDrawer: FC<Props> = ({ crowi }) => {
   const [t] = useTranslation()
 
   return (
-    <div className="v2-crowi-global-menu-container" id="crowi-global-menu">
-      <div className="v2-crowi-global-menu">
+    <NavigationDrawerContainer className="v2-crowi-global-menu-container" id="crowi-global-menu">
+      <StyledNavigationDrawer>
         <nav className="navbar" role="navigation">
           <a className="navbar-brand" href="/">
             <img alt="Crowi" src="/logo/32x32i.png" width="16" />
@@ -24,8 +84,8 @@ const NavigationDrawer: FC<Props> = ({ crowi }) => {
           </a>
         </nav>
 
-        <div className="menu-user-profile">
-          <div className="d-flex  justify-content-between">
+        <UserProfile>
+          <div className="d-flex justify-content-between">
             <div className="menu-user-picture">
               <a href={getUserPageRoot(user)}>
                 <img src={getUserPicture(user)} className="picture picture-rounded" />
@@ -41,46 +101,46 @@ const NavigationDrawer: FC<Props> = ({ crowi }) => {
             </div>
           </div>
 
-          <div className="menu-user-names">
+          <Names>
             <a href={getUserPageRoot(user)}>
-              <span className="menu-user-name">{user?.name}</span>
+              <Name>{user?.name}</Name>
               <br />
-              <span className="menu-user-username">@{user?.username}</span>
+              <Username>@{user?.username}</Username>
             </a>
-          </div>
-        </div>
+          </Names>
+        </UserProfile>
 
-        <ul className="menu-list">
-          <li>
+        <Menu>
+          <MenuItem>
             <a href="/me">
               <Icon name="cogs" /> {t('User Settings')}
             </a>
-          </li>
-          <li>
+          </MenuItem>
+          <MenuItem>
             <a href={`${getUserPageRoot(user)}/bookmarks`}>
               <Icon name="star" /> {t('Bookmarks')}
             </a>
-          </li>
-          <li>
+          </MenuItem>
+          <MenuItem>
             <a href={`${getUserPageRoot(user)}/recent-create`}>
               <Icon name="pencilOutline" /> {t('Created pages')}
             </a>
-          </li>
-          <li className="divider"></li>
-          <li>
+          </MenuItem>
+          <Divider />
+          <MenuItem>
             <a href="/trash/">
               <Icon name="trashCanOutline" /> {t('Deleted Pages')}
             </a>
-          </li>
-          <li className="divider"></li>
-          <li>
+          </MenuItem>
+          <Divider />
+          <MenuItem>
             <a href="/logout">
               <Icon name="logout" /> {t('Sign out')}
             </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+          </MenuItem>
+        </Menu>
+      </StyledNavigationDrawer>
+    </NavigationDrawerContainer>
   )
 }
 
