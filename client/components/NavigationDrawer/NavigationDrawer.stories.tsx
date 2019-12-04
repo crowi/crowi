4034c9cc2f3a701b748c9c60a98f77701f3bde81
lightnet328 <lightnet328@gmail.com>
2019-12-05 02:44:44 +0900

    Add story for NavigationDrawer

---
+++ client/components/NavigationDrawer/NavigationDrawer.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavigationDrawer from './NavigationDrawer'
+import Crowi from '../../util/Crowi'
+import i18n from '../../i18n'
+
+i18n()
+
+const crowi = new Crowi({ user: { name: 'Crowi', username: 'crowi' }, csrfToken: '' }, window)
+
+export default { title: 'NavigationDrawer' }
+
+export const Default = () => <NavigationDrawer crowi={crowi} isOpen={true} />
