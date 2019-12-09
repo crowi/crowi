0071ed0e78f6075e9358a1e3b0ba239ef69f33e6
lightnet328 <lightnet328@gmail.com>
2019-12-09 22:01:40 +0900

    Move document elements

--- lib/pages/AdminPage.tsx
+++ lib/pages/AdminPage.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import Document from '../components/Document'
 import Single, { Props as SingleProps } from '../components/Layout/Single'
 
 type Props = SingleProps
@@ -6,14 +7,16 @@ type Props = SingleProps
 const AdminPage: FC<Props> = props => {
   const { context } = props
   return (
-    <Single title={`Wiki管理 · ${context.path}`} context={context}>
-      <div className="header-wrap">
-        <header id="page-header">
-          <h1 className="title">Wiki管理</h1>
-        </header>
-      </div>
-      <div id="admin-page" className="content-main content-form" data-csrftoken={context.csrfToken}></div>
-    </Single>
+    <Document title={`Wiki管理 · ${context.path}`} context={context}>
+      <Single context={context}>
+        <div className="header-wrap">
+          <header id="page-header">
+            <h1 className="title">Wiki管理</h1>
+          </header>
+        </div>
+        <div id="admin-page" className="content-main content-form" data-csrftoken={context.csrfToken}></div>
+      </Single>
+    </Document>
   )
 }
 
