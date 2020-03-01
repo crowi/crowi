3a42be67e0149d2a31c8a9a06b9a8d5c310a3651
lightnet328 <lightnet328@gmail.com>
2020-03-02 05:00:02 +0900

    Fix import path

--- client/components/Comment/CommentItem.tsx
+++ client/components/Comment/CommentItem.tsx
@@ -4,7 +4,7 @@ import UserPicture from 'components/User/UserPicture'
 import CommentBody from './CommentBody'
 import * as styles from 'client/constants/styles'
 import { CommonProps } from 'client/types/component'
-import Crowi from 'client/util/Crowi'
+import Crowi from 'client/utils/Crowi'
 import { formatToLocaleString, formatDistanceFromNow } from 'client/util/formatDate'
 
 type PageCommentContainerProps = Props & {
