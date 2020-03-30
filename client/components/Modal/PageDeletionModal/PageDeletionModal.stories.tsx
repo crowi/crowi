c0fb036d5e791441d8fbe7b966064a415e3e41ee
okonomi <okonomi@oknm.jp>
2020-03-30 22:21:41 +0900

    add story

---
+++ client/components/Modal/PageDeletionModal/PageDeletionModal.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PageDeletionModal from './PageDeletionModal'
+import Crowi from '../../../utils/Crowi'
+import { createAppContext } from 'client/fixtures/createAppContext'
+import { Button } from 'reactstrap'
+
+const appContext = createAppContext({ path: '/foo/bar' })
+const crowi = new Crowi(appContext, window)
+const pageId = null
+const revisionId = null
+
+export default { title: 'Modal/PageDeletionModal' }
+
+export const Default = () => (
+  <>
+    <Button color="primary" data-target="#deletePage">
+      Open Modal
+    </Button>
+    <PageDeletionModal crowi={crowi} pageId={pageId} revisionId={revisionId} />
+  </>
+)
