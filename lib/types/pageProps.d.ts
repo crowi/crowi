9578c116abea8bb0eeadfb02a90bd9d486518041
lightnet328 <lightnet328@gmail.com>
2020-04-29 16:21:58 +0900

    Fix type errors

--- lib/types/pageProps.d.ts
+++ lib/types/pageProps.d.ts
@@ -1,7 +1,7 @@
-import i18next from 'i18next'
+import { i18n} from 'i18next'
 import { AppContext } from './appContext'
 
 export type PageProps = {
-  i18n: i18next.i18n
+  i18n: i18n
   context: AppContext
 }
