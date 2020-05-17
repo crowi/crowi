d8117d9d98f00f8d288065e1a100ae40bb46f1b1
Sotaro KARASAWA <sotaro.k@gmail.com>
2020-05-17 13:40:01 +0900

    fix: pagination spec adjust for assumed behavior

--- client/components/Admin/User/UserTable.tsx
+++ client/components/Admin/User/UserTable.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import Pagination from 'components/Common/Pagination'
+import Pagination, { Pager } from 'components/Common/Pagination'
 import UserSearchForm from './UserSearchForm'
 import UserTableRow from './UserTableRow'
 import { Me } from 'client/utils/Crowi'
@@ -56,10 +56,7 @@ function getHanlders(openResetModal, changeStatus) {
 interface Props {
   me: Me
   users: any[]
-  pagination: {
-    current: number
-    count: number
-  }
+  pager?: Pager
   query: string
   setQuery: (query: string) => void
   search: (query: string) => void
@@ -68,9 +65,8 @@ interface Props {
   changeStatus: (user: any, string: string) => void
 }
 
-const UserTable: FC<Props> = ({ me, users, pagination, query, setQuery, search, move, openResetModal, changeStatus }) => {
+const UserTable: FC<Props> = ({ me, users, pager, query, setQuery, search, move, openResetModal, changeStatus }) => {
   const [t] = useTranslation()
-  const { current, count } = pagination
   const handlers = getHanlders(openResetModal, changeStatus)
   return (
     <>
@@ -96,7 +92,7 @@ const UserTable: FC<Props> = ({ me, users, pagination, query, setQuery, search, 
                 ))}
               </tbody>
             </table>
-            <Pagination current={current} count={count} onClick={move} />
+            <Pagination pager={pager} onClick={move} />
           </>
         ) : (
           <p>{t('admin.user.not_found')}</p>
--- client/components/Common/Pagination.tsx
+++ client/components/Common/Pagination.tsx
@@ -1,47 +1,103 @@
 import React from 'react'
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap'
 
-interface Props {
-  current: number
-  count: number
-  onClick: Function
+// it is the same as server's definition
+export interface Pager {
+  page: any
+  pagesCount: number
+  pages: number[]
+  total: number
+  previous: number | null
+  previousDots: boolean
+  next: number | null
+  nextDots: boolean
 }
 
-export default class PaginationWrapper extends React.Component<Props> {
-  onClick(i: number) {
-    const { onClick } = this.props
+interface Props {
+  onClick: Function
+  current?: number
+  count?: number
+  pager?: Pager
+}
+
+const PaginationWrapper: React.FC<Props> = props => {
+  const { current, count, pager } = props
+
+  const onClick = (i?: number | null) => {
+    const { onClick } = props
     return (e: React.MouseEvent<HTMLElement>) => {
       e.preventDefault()
-      if (onClick) {
-        onClick(i)
-      }
+      onClick(i)
     }
   }
 
-  render() {
-    const { current, count } = this.props
-    if (current < 1 || count < 1) {
+  // LegacyPagination
+  if (!pager) {
+    if (current && count && (current < 1 || count < 1)) {
       return null
     }
-    const range = [...Array(count).keys()]
-    const items = range.map((v, k) => {
-      const page = k + 1
-      return (
-        <PaginationItem key={page} active={page === current}>
-          <PaginationLink onClick={this.onClick(page)}>{page}</PaginationLink>
-        </PaginationItem>
-      )
-    })
+
     return (
       <Pagination>
         <PaginationItem disabled={current === 1}>
-          <PaginationLink previous onClick={this.onClick(1)} />
+          <PaginationLink previous onClick={onClick(1)} />
         </PaginationItem>
-        {items}
+        {[...Array(count).keys()].map((v, k) => {
+          const page = k + 1
+          return (
+            <PaginationItem key={page} active={page === current}>
+              <PaginationLink onClick={onClick(page)}>{page}</PaginationLink>
+            </PaginationItem>
+          )
+        })}
         <PaginationItem disabled={current === count}>
-          <PaginationLink next onClick={this.onClick(count)} />
+          <PaginationLink next onClick={onClick(count)} />
         </PaginationItem>
       </Pagination>
     )
   }
+
+  return (
+    <Pagination>
+      {pager.page !== 1 && (
+        <PaginationItem>
+          <PaginationLink first onClick={onClick(1)} />
+        </PaginationItem>
+      )}
+      {pager.previous && (
+        <PaginationItem>
+          <PaginationLink previous onClick={onClick(pager.previous)} />
+        </PaginationItem>
+      )}
+      {pager.previousDots && (
+        <PaginationItem>
+          <PaginationLink href="#">...</PaginationLink>
+        </PaginationItem>
+      )}
+      {pager.pages.map(p => {
+        return (
+          <PaginationItem key={p} active={p === pager.page}>
+            <PaginationLink onClick={onClick(p)}>{p}</PaginationLink>
+          </PaginationItem>
+        )
+      })}
+      {pager.nextDots && (
+        <PaginationItem>
+          <PaginationLink href="#">...</PaginationLink>
+        </PaginationItem>
+      )}
+      {pager.next && (
+        <PaginationItem>
+          <PaginationLink next onClick={onClick(pager.next)} />
+        </PaginationItem>
+      )}
+      {pager.page !== pager.pagesCount && (
+        <PaginationItem>
+          <PaginationLink last onClick={onClick(pager.pagesCount)} />
+        </PaginationItem>
+      )}
+    </Pagination>
+  )
 }
+
+export default PaginationWrapper
