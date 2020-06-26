9900c5866365a5ae862dfc122b119a5f3251b7d4
Sotaro KARASAWA <sotaro.k@gmail.com>
2020-06-26 18:45:48 +0900

    Move to function component

--- client/components/PageHistory/RevisionDiff.tsx
+++ client/components/PageHistory/RevisionDiff.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { createPatch } from 'diff'
-import * as Diff2Html from 'diff2html'
+import { html } from 'diff2html'
 
 import { Revision } from 'client/types/crowi'
 
@@ -11,27 +11,26 @@ interface Props {
   revisionDiffOpened: boolean
 }
 
-export default class RevisionDiff extends React.Component<Props> {
-  render() {
-    const { currentRevision, previousRevision, revisionDiffOpened } = this.props
-
-    let diffViewHTML = ''
-    if (currentRevision.body && previousRevision.body && revisionDiffOpened) {
-      let previousText = previousRevision.body
-      if (currentRevision._id == previousRevision._id) {
-        previousText = ''
-      }
-
-      const patch = createPatch(currentRevision.path, previousText, currentRevision.body, '', '')
-
-      const options = {
-        drawFileList: false,
-      }
-
-      diffViewHTML = Diff2Html.html(patch, options)
+const RevisionDiff: React.FC<Props> = ({ currentRevision, previousRevision, revisionDiffOpened }) => {
+  let diffViewHTML = ''
+  if (currentRevision.body && previousRevision.body && revisionDiffOpened) {
+    let previousText = previousRevision.body
+    if (currentRevision._id == previousRevision._id) {
+      previousText = ''
     }
 
-    const diffView = { __html: diffViewHTML }
-    return <div className="revision-history-diff" dangerouslySetInnerHTML={diffView} />
+    const patch = createPatch(currentRevision.path, previousText, currentRevision.body, '', '')
+
+    const options = {
+      drawFileList: false,
+    }
+
+    diffViewHTML = html(patch, options)
   }
+
+  const diffView = { __html: diffViewHTML }
+
+  return <div className="revision-history-diff" dangerouslySetInnerHTML={diffView} />
 }
+
+export default RevisionDiff
