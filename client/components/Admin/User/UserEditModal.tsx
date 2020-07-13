75e1973342dcfe79fb0a2617b5f46cb9003d256c
Ryohei Tsuda <lyohe@hey.com>
2020-07-13 16:39:03 +0900

    add alert for testing

--- client/components/Admin/User/UserEditModal.tsx
+++ client/components/Admin/User/UserEditModal.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react'
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap'
+import React, { useState, FC } from 'react'
+import { Form, FormGroup, FormText, Input, Label, Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap'
 
 interface Props {
   isOpen: boolean
@@ -8,24 +8,40 @@ interface Props {
 }
 
 const UserEditModal: FC<Props> = ({ isOpen, toggle, user = {} }) => {
-  const handleClick = (e) => {
-    e.preventDefault()
-    alert('編集！！！')
+  const [userName, setUserName] = useState()
+  const [email, setEmail] = useState()
+
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value)
+  }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    alert(`${userName} + ${email}`)
   }
   return (
     <Modal isOpen={isOpen} toggle={toggle}>
       <ModalHeader toggle={toggle}>ユーザー情報を編集しますか?</ModalHeader>
       <ModalBody>
-        <p>処理はこれから作ります。</p>
-        <p>
-          edited user: <code>{user.email}</code>
-        </p>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label>
+              現在のなまえ: <code>{user.name}</code>
+            </Label>
+            <Input type="text" name="userName" placeholder="Please enter your new user name" onChange={handleUserNameChange} />
+          </FormGroup>
+          <FormGroup>
+            <Label>
+              現在のメールアドレス: <code>{user.email}</code>
+            </Label>
+            <Input type="email" name="email" placeholder="Please enter your new email address" onChange={handleEmailChange} />
+          </FormGroup>
+          <Button type="submit" color="primary">
+            実行
+          </Button>
+        </Form>
       </ModalBody>
-      <ModalFooter>
-        <Button type="submit" color="primary" onClick={handleClick}>
-          実行
-        </Button>
-      </ModalFooter>
     </Modal>
   )
 }
