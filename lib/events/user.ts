ea7ed37d1a8d1f605c8381bfda626b9525d2d9e0
Tadao Iseki <saitoeku3@gmail.com>
2020-08-30 16:35:10 +0900

    feat: move manually created user page automatically

--- lib/events/user.ts
+++ lib/events/user.ts
@@ -1,6 +1,8 @@
 import Crowi from 'server/crowi'
 import { EventEmitter } from 'events'
 import Debug from 'debug'
+import { UserDocument } from 'server/models/user'
+
 const debug = Debug('crowi:events:user')
 
 export default class UserEvent extends EventEmitter {
@@ -11,25 +13,35 @@ export default class UserEvent extends EventEmitter {
     this.crowi = crowi
   }
 
-  async onActivated(user) {
+  async onActivated(user: UserDocument) {
     const Page = this.crowi.model('Page')
-
     const userPagePath = Page.getUserPagePath(user)
-    Page.findPage(userPagePath, user, {}, false)
-      .then(function (page) {
-        // do nothing because user page is already exists.
-      })
-      .catch(function (err) {
-        const body = `# ${user.username}\nThis is ${user.username}'s page`
-        // create user page
-        Page.createPage(userPagePath, body, user, {})
-          .then(function (page) {
-            // page created
-            debug('User page created', page)
-          })
-          .catch(function (err) {
-            debug('Failed to create user page', err)
-          })
-      })
+
+    try {
+      const page = await Page.findPage(userPagePath, user, {}, false)
+
+      if (page === null) {
+        throw new Error()
+      }
+
+      // User page is already exists.
+      const renamedUserPagePath = `/tmp/user-${user.username}-${Date.now()}`
+      await Page.rename(page, renamedUserPagePath, user, {})
+      await this.createUserPage(userPagePath, user)
+    } catch {
+      await this.createUserPage(userPagePath, user)
+    }
+  }
+
+  private async createUserPage(userPagePath: string, user: UserDocument) {
+    const Page = this.crowi.model('Page')
+    const body = `# ${user.username}\nThis is ${user.username}'s page`
+
+    try {
+      const page = await Page.createPage(userPagePath, body, user, {})
+      debug('User page created', page)
+    } catch (err) {
+      debug('Failed to create user page', err)
+    }
   }
 }
