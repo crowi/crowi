3797a7e7a70a4bba65ca7cb0475e2904705fe8c1
Ryohei Tsuda <lyohe@hey.com>
2020-09-12 15:38:35 +0900

    add i18n translation to user invite form in admin page

--- client/components/Admin/User/InviteUserForm.tsx
+++ client/components/Admin/User/InviteUserForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FC } from 'react'
 import { Collapse, Form, FormGroup, FormText, Label, Input, CustomInput, Button, Row, Col } from 'reactstrap'
+import { useTranslation } from 'react-i18next'
 
 function useForm(invite) {
   const [emails, setEmails] = useState('')
@@ -27,6 +28,7 @@ interface Props {
 }
 
 const InviteUserForm: FC<Props> = ({ invite }) => {
+  const [t] = useTranslation()
   const [collapse, setCollapse] = useState(false)
   const [{ emails, sendEmail }, { setEmails, setSendEmail, onSubmit }] = useForm(invite)
 
@@ -34,7 +36,7 @@ const InviteUserForm: FC<Props> = ({ invite }) => {
     <>
       <p>
         <Button color="secondary" onClick={() => setCollapse(!collapse)}>
-          新規ユーザーの招待
+          {t('admin.user.invite.form.collapse')}
         </Button>
       </p>
       <Form onSubmit={onSubmit}>
@@ -42,11 +44,17 @@ const InviteUserForm: FC<Props> = ({ invite }) => {
           <FormGroup>
             <Row>
               <Col xs={{ size: 2, offset: 1 }}>
-                <Label for="emails">メールアドレス</Label>
+                <Label for="emails">{t('admin.user.invite.form.email')}</Label>
               </Col>
               <Col xs="8">
-                <Input type="textarea" id="emails" placeholder="例: user@crowi.wiki" value={emails} onChange={(e) => setEmails(e.target.value)} />
-                <FormText color="muted">複数行入力で複数人招待可能</FormText>
+                <Input
+                  type="textarea"
+                  id="emails"
+                  placeholder={t('admin.user.invite.form.example')}
+                  value={emails}
+                  onChange={(e) => setEmails(e.target.value)}
+                />
+                <FormText color="muted">{t('admin.user.invite.form.description')}</FormText>
               </Col>
             </Row>
           </FormGroup>
@@ -54,7 +62,14 @@ const InviteUserForm: FC<Props> = ({ invite }) => {
           <FormGroup>
             <Row>
               <Col xs={{ size: 8, offset: 3 }}>
-                <CustomInput id="sendEmail" type="checkbox" label="招待をメールで送信" checked={sendEmail} onChange={() => setSendEmail(!sendEmail)} inline />
+                <CustomInput
+                  id="sendEmail"
+                  type="checkbox"
+                  label={t('admin.user.invite.form.checkbox')}
+                  checked={sendEmail}
+                  onChange={() => setSendEmail(!sendEmail)}
+                  inline
+                />
               </Col>
             </Row>
           </FormGroup>
@@ -62,7 +77,7 @@ const InviteUserForm: FC<Props> = ({ invite }) => {
           <FormGroup>
             <Row>
               <Col xs={{ size: 8, offset: 3 }}>
-                <Button color="primary">招待する</Button>
+                <Button color="primary">{t('admin.user.invite.form.submit')}</Button>
               </Col>
             </Row>
           </FormGroup>
