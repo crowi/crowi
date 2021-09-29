942d0722d574c869bab830ed8ca7706915176fdc
Sotaro KARASAWA <sotaro.k@gmail.com>
2021-09-29 22:24:23 +0900

    fix: storybook middleware

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,3 +1,3 @@
-const proxy = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = router => router.use('/_api', proxy({ target: 'http://0.0.0.0:3000', changeOrigin: true }))
+module.exports = router => router.use('/_api', createProxyMiddleware({ target: 'http://0.0.0.0:3000', changeOrigin: true }))
