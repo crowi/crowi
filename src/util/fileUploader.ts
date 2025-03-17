f9049c86e8f669ea993b1dc2b10585be2e1f8668
Sotaro KARASAWA <sotaro.k@gmail.com>
2025-03-17 13:51:44 +0900

    replace fileUploader implementation with dummy

--- src/util/fileUploader.ts
+++ src/util/fileUploader.ts
@@ -1,10 +1,20 @@
 import Crowi from 'src/crowi'
 
+const fileUploader = {
+  async uploadFile(filePath: string, type: string, fileStream: any, options: any) {
+    throw new Error('Not implemented')
+  },
+  generateUrl(filePath: string) {
+    throw new Error('Not implemented')
+  },
+}
+
 export default (crowi: Crowi) => {
   'use strict'
 
   // var debug = Debug('crowi:lib:fileUploader')
-  const method = crowi.env.FILE_UPLOAD || 'aws'
+  //const method = crowi.env.FILE_UPLOAD || 'aws'
 
-  return require('../../local_modules/crowi-fileupload-' + method + '/index.js')(crowi)
+  //return require('../../local_modules/crowi-fileupload-' + method + '/index.js')(crowi)
+  return fileUploader
 }
