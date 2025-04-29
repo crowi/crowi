0ecdfeb63defd7e514725c27aeca1a79b441e938
Sotaro KARASAWA <sotaro.k@gmail.com>
2025-04-29 14:27:18 +0900

    fix route functions

--- src/routes/me.ts
+++ src/routes/me.ts
@@ -8,11 +8,11 @@ export default (crowi: Crowi, app: Express, form) => {
 
   router.use('/me*', LoginRequired)
 
-  router.get('/me', Me.index)
+  router.get('/me', Me.user)
   router.get('/me/password', Me.password)
   router.get('/me/apiToken', Me.apiToken)
   router.get('/me/auth/third-party', Me.authThirdParty)
-  router.post('/me', form.me.user, Me.index)
+  router.post('/me', form.me.user, Me.user)
   router.post('/me/password', form.me.password, Me.password)
   router.post('/me/apiToken', form.me.apiToken, Me.apiToken)
   router.get('/me/notifications', Me.notifications)
@@ -21,6 +21,7 @@ export default (crowi: Crowi, app: Express, form) => {
   router.get('/me/auth/google/callback', Me.authGoogleCallback)
   router.post('/me/auth/github', Me.authGitHub)
   router.get('/me/auth/github/callback', Me.authGitHubCallback)
+
 
   return router
 }
