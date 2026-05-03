aa8fac5ac249b180208da4dcd2a5985e11c057ce
npt-1707 <npthanh132@gmail.com>
2026-05-04 07:37:03 +0800

    apps/crowi-api/public/js/reveal/plugin/markdown/markdown.js: changed the SCRIPT_END_PLACEHOLDER regex to case insensitive

--- apps/crowi-api/public/js/reveal/plugin/markdown/markdown.js
+++ apps/crowi-api/public/js/reveal/plugin/markdown/markdown.js
@@ -112,7 +112,7 @@
 
 		// prevent script end tags in the content from interfering
 		// with parsing
-		content = content.replace( /<\/script>/g, SCRIPT_END_PLACEHOLDER );
+		content = content.replace( /<\/script>/gi, SCRIPT_END_PLACEHOLDER );
 
 		return '<script type="text/template">' + content + '</script>';
 
